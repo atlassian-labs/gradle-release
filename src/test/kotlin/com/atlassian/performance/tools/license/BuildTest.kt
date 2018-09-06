2b543f3bd9c686908a0de999ce3829b1c09af080
Sebastian Pawlak <spawlak@atlassian.com>
2018-09-06 09:53:13 +0200

    JPERF-58: Expose issue with test task being not executed when building a project using the plugin

---
+++ src/test/kotlin/com/atlassian/performance/tools/license/BuildTest.kt
@@ -0,0 +1,55 @@
+package com.atlassian.performance.tools.license
+
+import org.gradle.testkit.runner.GradleRunner
+import org.gradle.testkit.runner.TaskOutcome.SUCCESS
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+import java.io.File
+
+class BuildTest {
+
+    @Test
+    fun shouldExecuteTestTask() {
+        val result = GradleRunner.create()
+            .withProjectDir(configureBuildGradle())
+            .withArguments("build")
+            .withPluginClasspath()
+            .withDebug(true)
+            .build()
+
+        assertEquals(SUCCESS, result.task(":test")!!.outcome)
+    }
+
+    private fun configureBuildGradle(): File {
+        val buildFolder = TemporaryFolder()
+        buildFolder.create()
+        val buildGradle = buildFolder.newFile("build.gradle")
+        buildGradle.writeText(
+            """
+            plugins {
+                id 'com.atlassian.performance.tools.gradle-release'
+                id "org.jetbrains.kotlin.jvm" version "1.2.61"
+            }
+
+            dependencies {
+                compile 'org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.2.61'
+                testCompile 'junit:junit:4.12'
+            }
+        """.trimIndent()
+        )
+        val javaFolder = buildFolder
+            .newFolder("src", "test", "kotlin")
+
+        val javaClass = File(javaFolder, "Hello.kt")
+        javaClass.writeText("""
+            class HelloTest {
+                @org.junit.Test
+                fun test() {
+                }
+            }
+        """.trimIndent())
+
+        return buildFolder.root
+    }
+}
